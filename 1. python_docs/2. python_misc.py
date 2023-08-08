381680d944f3875edda1046cd661a71e99e8c287
Muhammad Hammad Sani <m.hammadsaani@gmail.com>
2023-08-09 01:07:48 +0500

    Module 2 and 3 added

---
+++ 1. python_docs/2. python_misc.py
@@ -0,0 +1,19 @@
+### interactive mode vs script execution mode
+
+## Underscore _ in interactive mode
+
+##  -c and -m flags (some modules are useful as scripts like http.server)
+
+## i flag can be used to enter in module interactive mode after script has been executed
+
+
+### sys and vector/list of arguments
+
+# import sys
+
+# print(sys.argv[0]) # file name
+
+# for i in sys.argv:
+#     print(i)
+    
+    
---
+++ 1. python_docs/3.intro_to_python.py
@@ -0,0 +1,55 @@
+### Floor Division vs int()
+
+## Floor division is almost similar to converting a float to integer using int()
+
+# print(10//3)
+# print(int(10/3))
+
+## Floor division is not same as floor and ceil math operations
+
+# import math 
+# num = 10.5
+# print(math.floor(num))
+# print(math.ceil(num))
+
+
+### Full Sopport for Floating Points
+
+## if expression have floats and integers, then they all are treated as floats and operations are performed
+
+# expression = 2 * (9.2 * 4)
+# print(expression)
+
+### Strings 
+
+## string concatenation 
+
+# text = 'a' 'b' # two strings next to each other
+# print(text)
+
+# text = (
+#         'a quick brown fox'
+#         'jumps over the lazy dog'
+#         )
+
+# print(text)
+
+### Lists
+
+## List concatenation
+
+# _list = [1, 2, 3]
+# _list = _list + [4, 5, 6]
+# print(_list)
+
+## replacing values in list
+
+# _list = [1, 2, 3, 4, 5]
+# _list[2:4] = [10, 20]
+# print(_list)
+
+## making list empty 
+
+# _list = [1, 2, 3, 4, 5]
+# _list[:] = []
+# print(_list)
